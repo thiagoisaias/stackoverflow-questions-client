ec8e754d3ce38cf8c38b43467196da489ada60a1
Thiago <thiagoisaiasbrg@gmail.com>
2018-06-23 20:32:45 -0300

    Update App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
+import Search from "../Search/Search";
+
+import { LayoutWrapper } from "../../shared/styled";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <LayoutWrapper>
+        <Search />
+      </LayoutWrapper>
     );
   }
 }
