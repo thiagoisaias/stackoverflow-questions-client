bce37a45e3dea0be9d2ec52d9be00e884bb3352f
Thiago <thiagoisaiasbrg@gmail.com>
2018-06-23 23:28:27 -0300

    Create mock question list

---
+++ src/questions.js
@@ -0,0 +1,174 @@
+export default {
+  items: [
+    {
+      tags: ["java", "c++", "performance", "optimization", "branch-prediction"],
+      owner: {
+        reputation: 285269,
+        user_id: 87234,
+        user_type: "registered",
+        accept_rate: 100,
+        profile_image: "https://i.stack.imgur.com/FkjBe.png?s=128&g=1",
+        display_name: "GManNickG",
+        link: "https://stackoverflow.com/users/87234/gmannickg"
+      },
+      is_answered: true,
+      view_count: 1213098,
+      protected_date: 1399067470,
+      accepted_answer_id: 11227902,
+      answer_count: 21,
+      score: 21456,
+      last_activity_date: 1529478191,
+      creation_date: 1340805096,
+      last_edit_date: 1522083910,
+      question_id: 11227809,
+      link:
+        "https://stackoverflow.com/questions/11227809/why-is-it-faster-to-process-a-sorted-array-than-an-unsorted-array",
+      title:
+        "Why is it faster to process a sorted array than an unsorted array?"
+    },
+    {
+      tags: ["javascript", "jquery", "redirect"],
+      owner: {
+        reputation: 48729,
+        user_id: 44984,
+        user_type: "registered",
+        accept_rate: 78,
+        profile_image: "https://i.stack.imgur.com/7rqob.jpg?s=128&g=1",
+        display_name: "venkatachalam",
+        link: "https://stackoverflow.com/users/44984/venkatachalam"
+      },
+      is_answered: true,
+      view_count: 4893999,
+      protected_date: 1447333882,
+      accepted_answer_id: 506004,
+      answer_count: 67,
+      score: 7739,
+      locked_date: 1522910705,
+      last_activity_date: 1526521941,
+      creation_date: 1233579256,
+      last_edit_date: 1517920814,
+      question_id: 503093,
+      link:
+        "https://stackoverflow.com/questions/503093/how-do-i-redirect-to-another-webpage",
+      title: "How do I redirect to another webpage?"
+    },
+    {
+      tags: ["javascript", "function", "variables", "scope", "closures"],
+      owner: {
+        reputation: 330008,
+        user_id: 9951,
+        user_type: "registered",
+        accept_rate: 78,
+        profile_image:
+          "https://www.gravatar.com/avatar/1449ec95d8c114dbca53136cc8194b61?s=128&d=identicon&r=PG&f=1",
+        display_name: "e-satis",
+        link: "https://stackoverflow.com/users/9951/e-satis"
+      },
+      is_answered: true,
+      view_count: 1244225,
+      protected_date: 1316450131,
+      accepted_answer_id: 111111,
+      answer_count: 88,
+      community_owned_date: 1370514175,
+      score: 7654,
+      locked_date: 1497372465,
+      last_activity_date: 1528374072,
+      creation_date: 1222006327,
+      last_edit_date: 1491746117,
+      question_id: 111102,
+      link:
+        "https://stackoverflow.com/questions/111102/how-do-javascript-closures-work",
+      title: "How do JavaScript closures work?"
+    },
+    {
+      tags: [
+        "javascript",
+        "string",
+        "substring",
+        "contains",
+        "string-matching"
+      ],
+      owner: {
+        reputation: 9167,
+        user_id: 131679,
+        user_type: "registered",
+        accept_rate: 63,
+        profile_image:
+          "https://www.gravatar.com/avatar/4cb7f78dca34d0e27931f743f3aa1f30?s=128&d=identicon&r=PG",
+        display_name: "gramm",
+        link: "https://stackoverflow.com/users/131679/gramm"
+      },
+      is_answered: true,
+      view_count: 4810683,
+      protected_date: 1351859467,
+      accepted_answer_id: 1789952,
+      answer_count: 49,
+      community_owned_date: 1388363490,
+      score: 7437,
+      locked_date: 1522910801,
+      last_activity_date: 1523635025,
+      creation_date: 1259067869,
+      last_edit_date: 1521639502,
+      question_id: 1789945,
+      link:
+        "https://stackoverflow.com/questions/1789945/how-to-check-whether-a-string-contains-a-substring-in-javascript",
+      title: "How to check whether a string contains a substring in JavaScript?"
+    },
+    {
+      tags: ["javascript", "jquery", "dom", "visibility"],
+      owner: {
+        reputation: 52279,
+        user_id: 21709,
+        user_type: "registered",
+        accept_rate: 100,
+        profile_image:
+          "https://www.gravatar.com/avatar/3ea9c684a2db319df788cecc6c98f9cd?s=128&d=identicon&r=PG",
+        display_name: "Philip Morton",
+        link: "https://stackoverflow.com/users/21709/philip-morton"
+      },
+      is_answered: true,
+      view_count: 2179012,
+      protected_date: 1334286591,
+      accepted_answer_id: 178450,
+      answer_count: 53,
+      score: 6631,
+      last_activity_date: 1524633661,
+      creation_date: 1223384598,
+      last_edit_date: 1477843243,
+      question_id: 178325,
+      link:
+        "https://stackoverflow.com/questions/178325/how-do-i-check-if-an-element-is-hidden-in-jquery",
+      title: "How do I check if an element is hidden in jQuery?"
+    },
+    {
+      tags: ["javascript", "syntax", "jslint", "use-strict"],
+      owner: {
+        reputation: 39415,
+        user_id: 25847,
+        user_type: "registered",
+        accept_rate: 82,
+        profile_image:
+          "https://www.gravatar.com/avatar/34d142b034e7b194fc1708f3b20d9568?s=128&d=identicon&r=PG",
+        display_name: "Mark Rogers",
+        link: "https://stackoverflow.com/users/25847/mark-rogers"
+      },
+      is_answered: true,
+      view_count: 951926,
+      protected_date: 1370840538,
+      accepted_answer_id: 1335881,
+      answer_count: 26,
+      score: 6624,
+      last_activity_date: 1524670615,
+      creation_date: 1251303013,
+      last_edit_date: 1485135002,
+      question_id: 1335851,
+      link:
+        "https://stackoverflow.com/questions/1335851/what-does-use-strict-do-in-javascript-and-what-is-the-reasoning-behind-it",
+      title:
+        "What does &quot;use strict&quot; do in JavaScript, and what is the reasoning behind it?"
+    }
+  ],
+  has_more: true,
+  quota_max: 10000,
+  quota_remaining: 9993
+};
