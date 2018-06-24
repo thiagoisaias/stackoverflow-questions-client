be3287dd8682165626576884792624829a2e0c02
Thiago <thiagoisaiasbrg@gmail.com>
2018-06-24 00:39:58 -0300

    Add mockProps to tests

--- src/components/Questions/Item/specs/Item.spec.js
+++ src/components/Questions/Item/specs/Item.spec.js
@@ -4,7 +4,22 @@ import toJson from "enzyme-to-json";
 import Item from "../Item";
 
 describe("Item", () => {
-  const component = shallow(<Item />);
+  const mockProps = {
+    id: 534,
+    createdAt: "12'17",
+    numberOfAnswers: 54,
+    owner: {
+      id: 234,
+      name: "Lorem",
+      profileImage: "http://someurl.com"
+    },
+    score: 568,
+    tags: ["tag1", "tag2"],
+    title: "This is an example!",
+    url: "http://thisisanurl.com"
+  };
+
+  const component = shallow(<Item {...mockProps} />);
 
   it("renders properly", () => {
     expect(toJson(component)).toMatchSnapshot();
--- src/components/Questions/List/specs/List.spec.js
+++ src/components/Questions/List/specs/List.spec.js
@@ -4,7 +4,40 @@ import toJson from "enzyme-to-json";
 import List from "../List";
 
 describe("List", () => {
-  const component = shallow(<List />);
+  const mockProps = {
+    isLoading: false,
+    questionList: [
+      {
+        id: 534,
+        createdAt: "12'17",
+        numberOfAnswers: 54,
+        owner: {
+          id: 234,
+          name: "Lorem",
+          profileImage: "http://someurl.com"
+        },
+        score: 568,
+        tags: ["tag1", "tag2"],
+        title: "This is an example!",
+        url: "http://thisisanurl.com"
+      },
+      {
+        id: 144,
+        createdAt: "13'17",
+        numberOfAnswers: 59,
+        owner: {
+          id: 134,
+          name: "Foo",
+          profileImage: "http://someurl.com"
+        },
+        score: 968,
+        tags: ["tag3", "tag4"],
+        title: "This is another example!",
+        url: "http://thisisanurl.com"
+      }
+    ]
+  }
+  const component = shallow(<List {...mockProps} />);
 
   it("renders properly", () => {
     expect(toJson(component)).toMatchSnapshot();
